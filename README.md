adaa3d43541d181979edaf41efebeb3c73fa6f9d
DavidClegg <i.am.david.clegg@gmail.com>
2021-01-01 15:12:59 +0000

    Update the colours and interactions

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ I can swipe to set the colour based on the distance I swipe,
 and if I swipe over half the width of the element then it is removed from the document.
 
 I think I'm in a position where I can add this to other projects, and refine my methodology.
+
+---
+
+Updated to improve the interaction and colours. Now it changes to a random colour on touch,
+yellow if it is moved but remains in the centre,
+red if it is to the right and will activate the right action (delete in this case),
+and blue if it is to the left and will activate the left action (also delete).
--- script.js
+++ script.js
@@ -1,129 +1,80 @@
-let boxes = document.querySelectorAll(".box");
-let w = window.screen.availWidth;
+let w = window.screen.availWidth; // The screen width
 //alert(w)
-let colours = [
-    "#4ad",
-    "#e7b",
-    "#765",
-    "#e51",
-    "#088"
-];
-for(box of boxes)
+function addEvents()
 {
+  let boxes = document.querySelectorAll(".box");
+  for(box of boxes)
+  {
     box.addEventListener("click", e=>{
-        console.log(e.target);
+      //console.log(e.target);
     });
     box.addEventListener("touchstart", e=>{
-        var initX = e.touches[0].pageX;
-        var initY = e.touches[0].pageY;
+      var initX = e.touches[0].pageX;
+      var initY = e.touches[0].pageY;
+      var endX = initX;
+      let log = document.querySelector("#log");
+      log.innerText = `Start: ${initX} \n`
+      e.target.addEventListener("touchmove", e=>{
+        var i;
+        var x;
+        var y;
+        for(i in e.changedTouches)
+        {
+          x = e.changedTouches[i].pageX;
+          if(i%3==0)
+          {
+            endX = x;
+            log.innerText += `Current: ${x} \n`
+            e.target.style.setProperty("left", `${x-initX}px`)
 
-        let log = document.querySelector("#log");
-        log.innerText = `Start: ${initX} \n`
-        e.target.addEventListener("touchmove", e=>{
-            var i;
-            var x;
-            var y;
-            for(i in e.changedTouches)
+            let cx = e.target.offsetLeft;
+            let cw = {p:e.target.offsetWidth*0.3, n:-(e.target.offsetWidth*0.3)};
+            if(cx > cw.p)
             {
-                x = e.changedTouches[i].pageX;
-                if(i%3==0)
-                {
-                    log.innerText += `Current: ${x} \n`
-                    if(x < initX)
-                    {
-                        log.innerText = "Left \n"  + `${initX - x}`
-                        e.target.style.background=`hsl(${parseInt(x-initX)*2}, 60%, 50%)`;
-                        e.target.style.setProperty("left", `${x-initX}px`)
-                    } else if(x > initX){
-                        log.innerText = "Right \n" + `${x - initX}`
-                        e.target.style.background=`hsl(${parseInt(x-initX)*2}, 60%, 50%)`;
-                        e.target.style.setProperty("left", `${x-initX}px`)
-                    }
-                    /*
-                    let log = document.querySelector("#log");
-                    //console.log(e)
-                    x = e.changedTouches[i].pageX
-                    y = e.changedTouches[i].pageY
-                    log.innerText = `${x}| ${y}` */
-                }
+              log.innerText = "Right \n"  + `cx:${cx} | cw:${cw.p}`
+              e.target.style.background=`#f00`;
+            } else if( cx < cw.n)
+            {
+              log.innerText = "Left \n" + `cx:${cx} | cw:${cw.n}`
+              e.target.style.background=`#00f`;
+            } else {
+              e.target.style.background=`#fa0`;
+              log.innerText = "Center \n" + `cx:${cx} | cw:${cw.p}`
             }
-        });
-        e.target.addEventListener("touchend", e=>{
-            for(b of boxes){
-                let offset = b.offsetLeft;
-                console.log(offset);
-                if(offset > (w*0.4) || offset < -(w*0.4))
-                {
-                    b.remove();
-                } else {
-                    b.style.left = 0;
-                }
-            }
-        });
+          }
+        }
+      });
+      e.target.addEventListener("touchend", e=>{
+        let thisBox = e.target;
+        let offset = thisBox.offsetLeft;
+        let boxWidth = thisBox.offsetWidth;
+        //console.log(`${initX}:>${endX}`)
+        if(initX == endX) // Not moved
+        {
+         thisBox.style.background =
+         `rgb(${rnd255()},${rnd255()},${rnd255()})`
+        }
+        if(offset > boxWidth*0.3 || offset < -(boxWidth*0.3))
+        {
+          thisBox.remove();
+        } else {
+          thisBox.style.left = 0;
+        }
+      });
     })
+  }
 }
 
-/*
-  EVERYTHING BELOW HERE IS JUST TO CHAGE THE COLOUR ON TAP
-  WHICH I COULD HAVE DONE MUCH MORE EASILY OR OTHERWISE DONE WITHOUT
-  IT ISN'T REALLY PART OF THE WHOLE SWIPING THING
-*/
+const rnd255 = () => parseInt(Math.random()*255)
 
+document.querySelector("button").addEventListener("click", e=>{
+    let child = document.createElement("div");
+    child.className = "box";
+    document.querySelector("#boxes").appendChild(child);
+    addEvents();
+})
 
-
-
-let box1 = document.querySelector("#box1");
-let box1Index = 0;
-box1.style.background = colours[box1Index];
-box1.addEventListener("click", e=>{
-    console.log(e);
-    box1Index = (box1Index + 1) % 5;
-    box1.style.background = colours[box1Index];
-});
-
-let box2 = document.querySelector("#box2");
-let box2Index = 0;
-box2.style.background = colours[box2Index];
-box2.addEventListener("click", e=>{
-    console.log(e);
-    box2Index = (box2Index + 1) % 5;
-    box2.style.background = colours[box2Index];
-});
-
-let box3 = document.querySelector("#box3");
-let box3Index = 0;
-box3.style.background = colours[box3Index];
-box3.addEventListener("click", e=>{
-    console.log(e);
-    box3Index = (box3Index + 1) % 5;
-    box3.style.background = colours[box3Index];
-});
-
-let box4 = document.querySelector("#box4");
-let box4Index = 0;
-box4.style.background = colours[box4Index];
-box4.addEventListener("click", e=>{
-    console.log(e);
-    box4Index = (box4Index + 1) % 5;
-    box4.style.background = colours[box4Index];
-});
-
-let box5 = document.querySelector("#box5");
-let box5Index = 0;
-box5.style.background = colours[box5Index];
-box5.addEventListener("touchstart", e=>{
-    console.log(e);
-    box5Index = (box5Index + 1) % 5;
-    box5.style.background = colours[box5Index];
-});
-
-function getEvent(event)
-{
-    let log = document.querySelector("#log");
-    console.log(event)
-    log.innerText = event.type// + "\n" +log.innerText;//.type
-}
-
+window.onload = () => addEvents();
 //document.addEventListener("touchstart", getEvent);
 /*
     This is always called when contact is made
